1a01631d4fafb25dba08704bbb18c3b2bebea530
aathiraj <119093795+VABBURS@users.noreply.github.com>
2023-09-15 15:06:06 +0530

    Add files via upload

---
+++ day1/Day 3/decoration/src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+     <i> <h1 className='vvd'>Dark Roast Events</h1></i>
+      
+    </header>
+  );
+};
+
+export default Header;
