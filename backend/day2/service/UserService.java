e959d43be63ab56dc20c9b6ee63f1b772fe100cf
aathiraj <119093795+VABBURS@users.noreply.github.com>
2023-10-06 09:29:50 +0530

    Add files via upload

---
+++ backend/day2/service/UserService.java
@@ -0,0 +1,7 @@
+package com.events.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService {
+UserDetailsService userDetailsService();
+}
