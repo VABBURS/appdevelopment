d68f8fbfca2a5427d552cf70762391006c862200
aathiraj <119093795+VABBURS@users.noreply.github.com>
2023-10-13 14:32:53 +0530

    Add files via upload

---
+++ backend/day9/src/main/java/com/pgadmission/main/dto/request/AuthenticationRequest.java
@@ -0,0 +1,14 @@
+package com.pgadmission.main.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private String password;
+}
